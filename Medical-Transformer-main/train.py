1d93892bb7650bf1f678a50dff0bbc3e147bef2d
JohnJYC <1962638769@qq.com>
2024-10-01 21:03:51 +0900

    更新了VAl acc计算的准确度问题，改良了图像读取

--- Medical-Transformer-main/train.py
+++ Medical-Transformer-main/train.py
@@ -6,23 +6,13 @@ import torchvision
 from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from torchvision import transforms
-from torchvision.utils import save_image
 import torch.nn.functional as F
 import os
-import matplotlib.pyplot as plt
-import torch.utils.data as data
-from PIL import Image
 import numpy as np
-from torchvision.utils import save_image
-import torch
-import torch.nn.init as init
 from utils import JointTransform2D, ImageToImage2D, Image2D
 from metrics import jaccard_index, f1_score, LogNLLLoss, classwise_f1
 from utils import chk_mkdir, Logger, MetricList
 import cv2
-from functools import partial
-from random import randint
 import timeit
 
 # 导入 wandb
@@ -89,9 +79,9 @@ train_dataset = ImageToImage2D(args.train_dataset, tf_train)
 val_dataset = ImageToImage2D(args.val_dataset, tf_val)
 predict_dataset = Image2D(args.val_dataset)
 dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-valloader = DataLoader(val_dataset, 1, shuffle=True)
+valloader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 
-device = torch.device("cuda")
+device = torch.device(args.device if torch.cuda.is_available() else "cpu")
 
 if modelname == "axialunet":
     model = lib.models.axialunet(img_size=imgsize, imgchan=imgchant)
@@ -104,12 +94,13 @@ elif modelname == "logo":
 
 if torch.cuda.device_count() > 1:
     print("Let's use", torch.cuda.device_count(), "GPUs!")
-    model = nn.DataParallel(model, device_ids=[0, 1]).cuda()
+    model = nn.DataParallel(model)
 model.to(device)
 
-criterion = LogNLLLoss()
-optimizer = torch.optim.Adam(list(model.parameters()), lr=args.learning_rate,
-                             weight_decay=1e-5)
+# 使用 CrossEntropyLoss 适用于多分类任务
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate,
+                             weight_decay=args.weight_decay)
 
 pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print("Total_params: {}".format(pytorch_total_params))
@@ -117,7 +108,8 @@ print("Total_params: {}".format(pytorch_total_params))
 seed = 3000
 np.random.seed(seed)
 torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
 
 # 初始化 wandb
 wandb.init(project='MedT', config={
@@ -128,27 +120,26 @@ wandb.init(project='MedT', config={
     'imgsize': args.imgsize,
     'gray': args.gray,
 })
-
 # 可选：监视模型
 wandb.watch(model, log='all')
 
-for epoch in range(args.epochs):
+for epoch in range(args.start_epoch, args.epochs):
 
+    model.train()
     epoch_running_loss = 0
 
     for batch_idx, (X_batch, y_batch, *rest) in enumerate(dataloader):
 
-        X_batch = Variable(X_batch.to(device='cuda'))
-        y_batch = Variable(y_batch.to(device='cuda'))
+        X_batch = X_batch.to(device)
+        y_batch = y_batch.to(device).squeeze(1).long()  # 标签形状调整
 
-        # ===================forward=====================
-
+        # 前向传播
         output = model(X_batch)
 
         # 计算损失
         loss = criterion(output, y_batch)
 
-        # ===================backward====================
+        # 反向传播和优化
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -158,78 +149,78 @@ for epoch in range(args.epochs):
     avg_loss = epoch_running_loss / (batch_idx + 1)
 
     # 记录指标到 wandb
-    wandb.log({'epoch': epoch, 'loss': avg_loss})
+    wandb.log({'epoch': epoch + 1, 'loss': avg_loss})
 
-    # ===================log========================
-    print('epoch [{}/{}], loss:{:.4f}'
-          .format(epoch, args.epochs, avg_loss))
+    # 打印日志
+    print('Epoch [{}/{}], Loss: {:.4f}'
+          .format(epoch + 1, args.epochs, avg_loss))
 
-    if epoch == 10:
-        for param in model.parameters():
-            param.requires_grad = True
-    if (epoch % args.save_freq) == 0:
+    if (epoch + 1) % args.save_freq == 0:
 
-        for batch_idx, (X_batch, y_batch, *rest) in enumerate(valloader):
-            # 获取文件名
-            if isinstance(rest[0][0], str):
-                image_filename = rest[0][0]
-            else:
-                image_filename = '%s.png' % str(batch_idx + 1).zfill(3)
+        model.eval()  # 设置模型为评估模式
+        total_correct = 0
+        total_pixels = 0
+        dice_scores = []
 
-            X_batch = Variable(X_batch.to(device='cuda'))
-            y_batch = Variable(y_batch.to(device='cuda'))
+        with torch.no_grad():
+            for batch_idx, (X_batch, y_batch, *rest) in enumerate(valloader):
+                # 获取文件名
+                if isinstance(rest[0][0], str):
+                    image_filename = rest[0][0]
+                else:
+                    image_filename = '%s.png' % str(batch_idx + 1).zfill(3)
 
-            y_out = model(X_batch)
+                X_batch = X_batch.to(device)
+                y_batch = y_batch.to(device).squeeze(1).long()  # 标签形状调整
 
-            tmp2 = y_batch.detach().cpu().numpy()
-            tmp = y_out.detach().cpu().numpy()
-            tmp[tmp >= 0.5] = 1
-            tmp[tmp < 0.5] = 0
-            tmp2[tmp2 > 0] = 1
-            tmp2[tmp2 <= 0] = 0
-            tmp2 = tmp2.astype(int)
-            tmp = tmp.astype(int)
+                y_out = model(X_batch)
+                y_pred = y_out.argmax(dim=1)  # 获取预测类别索引
 
-            yHaT = tmp
-            yval = tmp2
+                correct = (y_pred == y_batch).sum().item()
+                total = y_batch.numel()
+                accuracy = correct / total
 
-            # 计算变化率（作为示例指标）
-            variation_rate = np.mean(np.abs(yHaT - yval))
+                total_correct += correct
+                total_pixels += total
 
-            # 记录验证指标到 wandb
-            wandb.log({'variation_rate': variation_rate})
+                # 计算 Dice 系数
+                y_pred_np = y_pred.cpu().numpy()
+                y_true_np = y_batch.cpu().numpy()
+                smooth = 1e-5
+                intersection = (y_pred_np * y_true_np).sum()
+                dice = (2. * intersection + smooth) / (y_pred_np.sum() + y_true_np.sum() + smooth)
+                dice_scores.append(dice)
 
-            yHaT[yHaT == 1] = 255
-            yval[yval == 1] = 255
-            fulldir = direc + "/{}/".format(epoch)
-            if not os.path.isdir(fulldir):
-                os.makedirs(fulldir)
+                # 记录验证准确率
+                wandb.log({'val_accuracy': accuracy})
 
-            # 保存预测结果图像
-            cv2.imwrite(fulldir + image_filename, yHaT[0, 0, :, :])
+                # 保存预测结果图像
+                y_pred_image = (y_pred.cpu().numpy()[0] * 255).astype(np.uint8)
+                fulldir = os.path.join(direc, str(epoch + 1))
+                if not os.path.isdir(fulldir):
+                    os.makedirs(fulldir)
+                cv2.imwrite(os.path.join(fulldir, image_filename), y_pred_image)
 
-            # 检查数组形状
-            print("X_batch shape:", X_batch.cpu().numpy().shape)
-            print("yHaT shape:", yHaT.shape)
-            print("yval shape:", yval.shape)
+                # 记录图像到 wandb
+                wandb.log({
+                    'Input Image': wandb.Image(X_batch.cpu()[0]),
+                    'Prediction': wandb.Image(y_pred_image),
+                    'Ground Truth': wandb.Image((y_batch.cpu().numpy()[0] * 255).astype(np.uint8)),
+                })
 
-            # 根据数组形状调整索引
-            ground_truth_image = yval[0, :, :]
-            prediction_image = yHaT[0, 0, :, :]
+        # 计算整个验证集的平均准确率和 Dice 系数
+        avg_accuracy = total_correct / total_pixels
+        avg_dice = np.mean(dice_scores)
+        print(f'Validation Accuracy: {avg_accuracy:.4f}')
+        print(f'Validation Dice Coefficient: {avg_dice:.4f}')
+        wandb.log({'epoch': epoch + 1, 'val_avg_accuracy': avg_accuracy, 'val_dice_coefficient': avg_dice})
 
-            # 记录图像到 wandb
-            wandb.log({
-                'Input Image': wandb.Image(X_batch.cpu()[0]),
-                'Prediction': wandb.Image(prediction_image),
-                'Ground Truth': wandb.Image(ground_truth_image),
-            })
+        model.train()  # 重新设置模型为训练模式
 
-            # 在使用完变量后再删除它们
-            del X_batch, y_batch, tmp, tmp2, y_out
-
-        fulldir = direc + "/{}/".format(epoch)
-        torch.save(model.state_dict(), fulldir + args.modelname + ".pth")
-        torch.save(model.state_dict(), direc + "final_model.pth")
+        # 保存模型
+        fulldir = os.path.join(direc, str(epoch + 1))
+        torch.save(model.state_dict(), os.path.join(fulldir, args.modelname + ".pth"))
+        torch.save(model.state_dict(), os.path.join(direc, "final_model.pth"))
 
 # 结束 wandb 运行
 wandb.finish()
--- Medical-Transformer-main/utils_gray.py
+++ Medical-Transformer-main/utils_gray.py
@@ -2,14 +2,13 @@ import os
 import numpy as np
 import torch
 
-from skimage import io,color
+from skimage import io, color
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms as T
 from torchvision.transforms import functional as F
 
 from typing import Callable
-import os
 import cv2
 import pandas as pd
 
@@ -19,15 +18,16 @@ from collections import defaultdict
 
 
 def to_long_tensor(pic):
-    # handle numpy array
+    # 将 numpy 数组转换为长整型张量
     img = torch.from_numpy(np.array(pic, np.uint8))
-    # backward compatibility
     return img.long()
 
 
 def correct_dims(*images):
+    """
+    确保图像具有正确的维度。
+    """
     corr_images = []
-    # print(images)
     for img in images:
         if len(img.shape) == 2:
             corr_images.append(np.expand_dims(img, axis=2))
@@ -42,21 +42,16 @@ def correct_dims(*images):
 
 class JointTransform2D:
     """
-    Performs augmentation on image and mask when called. Due to the randomness of augmentation transforms,
-    it is not enough to simply apply the same Transform from torchvision on the image and mask separetely.
-    Doing this will result in messing up the ground truth mask. To circumvent this problem, this class can
-    be used, which will take care of the problems above.
+    对图像和掩码同时进行数据增强变换。
 
-    Args:
-        crop: tuple describing the size of the random crop. If bool(crop) evaluates to False, no crop will
-            be taken.
-        p_flip: float, the probability of performing a random horizontal flip.
-        color_jitter_params: tuple describing the parameters of torchvision.transforms.ColorJitter.
-            If bool(color_jitter_params) evaluates to false, no color jitter transformation will be used.
-        p_random_affine: float, the probability of performing a random affine transform using
-            torchvision.transforms.RandomAffine.
-        long_mask: bool, if True, returns the mask as LongTensor in label-encoded format.
+    参数：
+        crop: 随机裁剪的大小。如果为 False，则不进行裁剪。
+        p_flip: 执行随机水平翻转的概率。
+        color_jitter_params: torchvision.transforms.ColorJitter 的参数。
+        p_random_affine: 执行随机仿射变换的概率。
+        long_mask: 如果为 True，返回长整型的标签编码格式的掩码。
     """
+
     def __init__(self, crop=(32, 32), p_flip=0.5, color_jitter_params=(0.1, 0.1, 0.1, 0.1),
                  p_random_affine=0, long_mask=False):
         self.crop = crop
@@ -68,10 +63,10 @@ class JointTransform2D:
         self.long_mask = long_mask
 
     def __call__(self, image, mask):
-        # transforming to PIL image
+        # 转换为 PIL 图像
         image, mask = F.to_pil_image(image), F.to_pil_image(mask)
 
-        # random crop
+        # 随机裁剪
         if self.crop:
             i, j, h, w = T.RandomCrop.get_params(image, self.crop)
             image, mask = F.crop(image, i, j, h, w), F.crop(mask, i, j, h, w)
@@ -79,16 +74,18 @@ class JointTransform2D:
         if np.random.rand() < self.p_flip:
             image, mask = F.hflip(image), F.hflip(mask)
 
-        # color transforms || ONLY ON IMAGE
+        # 仅对图像进行颜色变换
         if self.color_jitter_params:
             image = self.color_tf(image)
 
-        # random affine transform
+        # 随机仿射变换
         if np.random.rand() < self.p_random_affine:
-            affine_params = T.RandomAffine(180).get_params((-90, 90), (1, 1), (2, 2), (-45, 45), self.crop)
-            image, mask = F.affine(image, *affine_params), F.affine(mask, *affine_params)
+            affine_params = T.RandomAffine.get_params(
+                degrees=(-90, 90), translate=(0.1, 0.1), scale_ranges=(0.9, 1.1), shears=(-10, 10), img_size=self.crop)
+            image = F.affine(image, *affine_params)
+            mask = F.affine(mask, *affine_params)
 
-        # transforming to tensor
+        # 转换为张量
         image = F.to_tensor(image)
         if not self.long_mask:
             mask = F.to_tensor(mask)
@@ -100,18 +97,17 @@ class JointTransform2D:
 
 class ImageToImage2D(Dataset):
     """
-    Reads the images and applies the augmentation transform on them.
-    Usage:
-        1. If used without the unet.model.Model wrapper, an instance of this object should be passed to
-           torch.utils.data.DataLoader. Iterating through this returns the tuple of image, mask and image
-           filename.
-        2. With unet.model.Model wrapper, an instance of this object should be passed as train or validation
-           datasets.
+    读取图像和掩码，并对其应用数据增强变换。
 
-    Args:
-        dataset_path: path to the dataset. Structure of the dataset should be:
+    用法：
+        1. 如果不使用 unet.model.Model 包装器，可以将该类的实例传递给 torch.utils.data.DataLoader。
+           迭代时返回图像、掩码和图像文件名的元组。
+        2. 使用 unet.model.Model 包装器时，可以将该类的实例作为训练或验证数据集传递。
+
+    参数：
+        dataset_path: 数据集的路径。数据集的结构应为：
             dataset_path
-              |-- images
+              |-- img
                   |-- img001.png
                   |-- img002.png
                   |-- ...
@@ -120,9 +116,8 @@ class ImageToImage2D(Dataset):
                   |-- img002.png
                   |-- ...
 
-        joint_transform: augmentation transform, an instance of JointTransform2D. If bool(joint_transform)
-            evaluates to False, torchvision.transforms.ToTensor will be used on both image and mask.
-        one_hot_mask: bool, if True, returns the mask in one-hot encoded form.
+        joint_transform: 增强变换，一个 JointTransform2D 的实例。如果没有提供，则对图像和掩码使用 torchvision.transforms.ToTensor。
+        one_hot_mask: bool，如果为 True，则返回 one-hot 编码形式的掩码。
     """
 
     def __init__(self, dataset_path: str, joint_transform: Callable = None, one_hot_mask: int = False) -> None:
@@ -139,66 +134,60 @@ class ImageToImage2D(Dataset):
             self.joint_transform = lambda x, y: (to_tensor(x), to_tensor(y))
 
     def __len__(self):
-        return len(os.listdir(self.input_path))
+        return len(self.images_list)
 
     def __getitem__(self, idx):
         image_filename = self.images_list[idx]
-        #print(image_filename[: -3])
-        # read image
-        # print(os.path.join(self.input_path, image_filename))
-        # print(os.path.join(self.output_path, image_filename[: -3] + "png"))
-        # print(os.path.join(self.input_path, image_filename))
-        image = cv2.imread(os.path.join(self.input_path, image_filename),0)
-        # print(image.shape)
-        # read mask image
-        mask = cv2.imread(os.path.join(self.output_path, image_filename[: -3] + "png"),0)
+        image_path = os.path.join(self.input_path, image_filename)
 
-        # correct dimensions if needed
+        # 获取不带扩展名的文件名
+        base_filename = os.path.splitext(image_filename)[0]
+        # 假设掩码文件名与图像文件名相同，扩展名为 .png
+        mask_filename = base_filename + ".png"
+        mask_path = os.path.join(self.output_path, mask_filename)
+
+        # 读取图像和掩码
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+
+        # 检查是否成功读取
+        if image is None:
+            raise FileNotFoundError(f"无法读取图像文件：{image_path}")
+        if mask is None:
+            raise FileNotFoundError(f"无法读取掩码文件：{mask_path}")
+
+        # 调整维度（如果需要）
         image, mask = correct_dims(image, mask)
-        # print(image.shape)
-        mask[mask<127] = 0
-        mask[mask>=127] = 1
 
+        # 二值化掩码
+        mask = (mask >= 127).astype(np.uint8)
 
+        # 应用联合变换
         if self.joint_transform:
             image, mask = self.joint_transform(image, mask)
 
+        # 如果需要，转换为 one-hot 编码
         if self.one_hot_mask:
-            assert self.one_hot_mask > 0, 'one_hot_mask must be nonnegative'
-            mask = torch.zeros((self.one_hot_mask, mask.shape[1], mask.shape[2])).scatter_(0, mask.long(), 1)
-        # mask = np.swapaxes(mask,2,0)
-        # print(image.shape)
-        # print(mask.shape)
-        # mask = np.transpose(mask,(2,0,1))
-        # image = np.transpose(image,(2,0,1))
-        # print(image.shape)
-        # print(mask.shape)
+            assert self.one_hot_mask > 0, 'one_hot_mask 必须是正整数'
+            mask = torch.nn.functional.one_hot(mask.squeeze().long(), num_classes=self.one_hot_mask)
+            mask = mask.permute(2, 0, 1).float()
 
         return image, mask, image_filename
 
 
 class Image2D(Dataset):
     """
-    Reads the images and applies the augmentation transform on them. As opposed to ImageToImage2D, this
-    reads a single image and requires a simple augmentation transform.
-    Usage:
-        1. If used without the unet.model.Model wrapper, an instance of this object should be passed to
-           torch.utils.data.DataLoader. Iterating through this returns the tuple of image and image
-           filename.
-        2. With unet.model.Model wrapper, an instance of this object should be passed as a prediction
-           dataset.
+    读取图像并对其应用数据增强变换。与 ImageToImage2D 相比，它只读取单个图像。
 
-    Args:
-        
-        dataset_path: path to the dataset. Structure of the dataset should be:
+    参数：
+        dataset_path: 数据集的路径。数据集的结构应为：
             dataset_path
-              |-- images
+              |-- img
                   |-- img001.png
                   |-- img002.png
                   |-- ...
 
-        transform: augmentation transform. If bool(joint_transform) evaluates to False,
-            torchvision.transforms.ToTensor will be used.
+        transform: 增强变换。如果未提供，则使用 torchvision.transforms.ToTensor。
     """
 
     def __init__(self, dataset_path: str, transform: Callable = None):
@@ -213,30 +202,35 @@ class Image2D(Dataset):
             self.transform = T.ToTensor()
 
     def __len__(self):
-        return len(os.listdir(self.input_path))
+        return len(self.images_list)
 
     def __getitem__(self, idx):
 
         image_filename = self.images_list[idx]
+        image_path = os.path.join(self.input_path, image_filename)
 
-        image = cv2.imread(os.path.join(self.input_path, image_filename),0)
+        # 读取图像
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-        # image = np.transpose(image,(2,0,1))
+        # 检查是否成功读取
+        if image is None:
+            raise FileNotFoundError(f"无法读取图像文件：{image_path}")
 
+        # 调整维度（如果需要）
         image = correct_dims(image)
 
+        # 应用变换
         image = self.transform(image)
-
-        # image = np.swapaxes(image,2,0)
 
         return image, image_filename
 
+
 def chk_mkdir(*paths: Container) -> None:
     """
-    Creates folders if they do not exist.
+    如果文件夹不存在，则创建它们。
 
-    Args:        
-        paths: Container of paths to be created.
+    参数：
+        paths: 要创建的路径的容器。
     """
     for path in paths:
         if not os.path.exists(path):
@@ -244,6 +238,10 @@ def chk_mkdir(*paths: Container) -> None:
 
 
 class Logger:
+    """
+    用于记录训练或验证过程中日志的类。
+    """
+
     def __init__(self, verbose=False):
         self.logs = defaultdict(list)
         self.verbose = verbose
@@ -263,21 +261,26 @@ class Logger:
 
 
 class MetricList:
+    """
+    用于计算和管理评估指标的类。
+    """
+
     def __init__(self, metrics):
-        assert isinstance(metrics, dict), '\'metrics\' must be a dictionary of callables'
+        assert isinstance(metrics, dict), '\'metrics\' 必须是一个包含可调用对象的字典'
         self.metrics = metrics
         self.results = {key: 0.0 for key in self.metrics.keys()}
 
     def __call__(self, y_out, y_batch):
-        for key, value in self.metrics.items():
-            self.results[key] += value(y_out, y_batch)
+        for key, func in self.metrics.items():
+            self.results[key] += func(y_out, y_batch)
 
     def reset(self):
         self.results = {key: 0.0 for key in self.metrics.keys()}
 
     def get_results(self, normalize=False):
-        assert isinstance(normalize, bool) or isinstance(normalize, Number), '\'normalize\' must be boolean or a number'
+        assert isinstance(normalize, bool) or isinstance(normalize, Number), \
+            '\'normalize\' 必须是布尔值或数字'
         if not normalize:
             return self.results
         else:
-            return {key: value/normalize for key, value in self.results.items()}
+            return {key: value / normalize for key, value in self.results.items()}
